171aef3f4bce0e96d02ce0a7d0834fc1c500a15f
shirin banoo <shirinkhorasanian@yahoo.com>
2022-10-12 09:41:12 +0300

    login package

--- src/App.tsx
+++ src/App.tsx
@@ -44,15 +44,7 @@ function App() {
     setHandle(!hanlde);
   }
 
-  function json(url: any) {
-    return fetch(url).then(res => res.json());
-  }
 
-  let apiKey = '40c4825cb3a4717869a82daa87ab7142259c8669097f3cf082fa138c';
-  json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
-    console.log(data);
-    // so many more properties
-  });
 
 
 
--- src/components/IpGeolocation/IntlTelField.tsx
+++ src/components/IpGeolocation/IntlTelField.tsx
@@ -2,6 +2,18 @@ import PhoneInput from 'react-phone-input-2'
 import 'react-phone-input-2/lib/style.css'
 
 const IpGeolocation = () => {
+
+  // function json(url: any) {
+  //   return fetch(url).then(res => res.json());
+  // }
+
+  // let apiKey = '40c4825cb3a4717869a82daa87ab7142259c8669097f3cf082fa138c';
+  // json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
+  //   console.log(data);
+  //   // so many more properties
+  // });
+
+
   return (
     <div>
       <PhoneInput
@@ -10,6 +22,7 @@ const IpGeolocation = () => {
           required: true,
           autoFocus: true
         }}
+        country={'tr'}
       />
     </div>
   )
